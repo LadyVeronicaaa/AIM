6350f0982197eb52205090605ce2cf1e5bbb58c8
LadyVeronica_Jay <142516130+LadyVeronicaaa@users.noreply.github.com>
2024-05-17 22:14:41 +0800

    done

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $conn = new mysqli("localhost", "root", "", "abcdb");
+    $conn = new mysqli("localhost", "root", "", "brms-laravel");
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
